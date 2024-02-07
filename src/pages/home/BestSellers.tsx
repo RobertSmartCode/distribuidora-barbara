abccb776f4840cb9917ee0ec8262a50f7c334e67
Robert Castillo <roca1388@gmail.com>
2024-02-07 13:50:59 -0300

    Listo para producción

--- src/pages/home/BestSellers.tsx
+++ src/pages/home/BestSellers.tsx
@@ -166,7 +166,7 @@ useEffect(() => {
                               sx={{
                                 position: "absolute",
                                 top: 0,
-                                left: isMobile ? -16 : -72, 
+                                left: isMobile ? -8 : -72, 
                                 backgroundColor: customColors.primary.main,
                                 color: customColors.secondary.contrastText,
                                 width: isMobile ? "32px" : "48px", 
