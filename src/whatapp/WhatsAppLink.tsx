e571d3979b4363cf4084567c638d0ced9f5ad091
Robert Castillo <roca1388@gmail.com>
2024-02-07 23:49:09 -0300

    Listo para producción

--- src/whatapp/WhatsAppLink.tsx
+++ src/whatapp/WhatsAppLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton, Tooltip } from '@mui/material';
+import { Tooltip } from '@mui/material';
 import { FaWhatsapp } from 'react-icons/fa';
 
 const WhatsAppLink: React.FC = () => {
@@ -8,40 +8,38 @@ const WhatsAppLink: React.FC = () => {
   const encodedMessage = encodeURIComponent(message);
   const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
 
-  const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    // Evita el comportamiento predeterminado de redireccionamiento
-    event.preventDefault();
+  const handleClick = () => {
     // Abre la URL de WhatsApp en una nueva ventana o pestaña
     window.open(whatsappURL, '_blank', 'noopener noreferrer');
   };
-
+  
   return (
     <Tooltip title="Enviar mensaje por WhatsApp">
-      <IconButton
-        component="a"
+      <a
         href={whatsappURL}
         target="_blank"
         rel="noopener noreferrer"
         onClick={handleClick} // Maneja el clic de forma personalizada
-        sx={{
+        style={{
           backgroundColor: '#25d366',
-          '&:hover': {
-            backgroundColor: '#25d366', // Mantén el mismo color de fondo al hacer hover
-          },
           color: 'white',
           borderRadius: '50%',
-          marginRight: { xs: '8px', md: '16px' },
+          padding: '10px', // Ajusta el padding según sea necesario
           position: 'fixed',
           bottom: '20px',
           right: '20px',
+          textDecoration: 'none', // Elimina la subrayado del enlace
           zIndex: 99,
+          display: 'flex',
+          alignItems: 'center',
         }}
       >
         <FaWhatsapp size={40} />
-      </IconButton>
+      </a>
     </Tooltip>
   );
 };
 
 export default WhatsAppLink;
+
 
