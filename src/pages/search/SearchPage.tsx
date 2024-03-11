5e0560fe4cffff5148f8706ac5185357447064d2
Robert Castillo <roca1388@gmail.com>
2024-03-11 12:57:07 -0300

    Banner

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -20,7 +20,7 @@ const SearchPage: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   const isMobile = useMediaQuery('(max-width: 600px)');
-  const maxTitleLength = isMobile ? 10 : 14;
+  const maxTitleLength = isMobile ? 10 : 10;
   const [clickedProduct, setClickedProduct] = useState<string | null>(null);
   const handleTitleClick = (title: string) => {
     setClickedProduct((prevClickedProduct) =>
@@ -174,7 +174,7 @@ const SearchPage: React.FC = () => {
                               sx={{
                                 position: "absolute",
                                 top: "0", // Ajusta la posición vertical
-                                left: isMobile ? "-70px" : "-140px",
+                                left: isMobile ? "-70px" : "-90px",
                                 backgroundColor: customColors.primary.main,
                                 color: customColors.secondary.contrastText,
                                 width: isMobile ? "36px" : "48px",
