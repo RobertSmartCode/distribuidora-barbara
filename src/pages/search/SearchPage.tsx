a1235a32117141e9cecaf0c96fdfabc7fb1addbb
Robert Castillo <roca1388@gmail.com>
2024-03-11 13:02:08 -0300

    Banner

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -151,7 +151,7 @@ const SearchPage: React.FC = () => {
       {searchResults.length > 0 ? (
        <div>
        <Typography variant="h6" gutterBottom sx={{ textAlign: 'center' }}>
-           Resultados de tu búsqueda.
+           Resultados de tu búsqueda. Prueba
          </Typography>
          {isComponentReady && (
    
