dd532bb18cb80b96d396c79cdea2713e95182138
Robert Castillo <roca1388@gmail.com>
2024-03-11 20:17:42 -0300

    update

--- src/pages/checkout/MercadoPagoPayment.tsx
+++ src/pages/checkout/MercadoPagoPayment.tsx
@@ -3,67 +3,48 @@ import { initMercadoPago, Wallet} from "@mercadopago/sdk-react";
 import axios from "axios";
 import { CartContext } from '../../context/CartContext';
 
-
+interface Product {
+  title: string;
+  unit_price: number;
+  quantity: number;
+}
 
 const MercadoPagoPayment = () => {
 
-  const { cart, getTotalPrice, } = useContext(CartContext)! || {};
-  const subtotal = getTotalPrice ? getTotalPrice() : 0;
+  const { cart } = useContext(CartContext)! || {};
+  
 
 
-  const [preferenceId, setPreferenceId] = useState(null);
+  const [preferenceId, setPreferenceId] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   const shippingCost = 0
  
-
- 
-  let total = (subtotal + shippingCost) 
-
-
-
-
-
-
   useEffect(() => {
     // Inicializa Mercado Pago con tu clave pública y la configuración de localización
     initMercadoPago(import.meta.env.VITE_PUBLICKEY, {
       locale: "es-AR",
     });
-
-    
     createPreference();
-
-
-    let order = {
-
-      items: cart,
-      shippingCost,
-    
-      total
-
-      
-    };
-    localStorage.setItem("order", JSON.stringify(order));
-   
   }, []);
 
-
   const createPreference = async () => {
-    const newArray = cart.map((product) => {
-      return {
-        title: product.title,
-        unit_price: product.price,
-        quantity: product.quantity,
-      };
-    });
-
+    let newArray: Product[]; // Declaración explícita del tipo de newArray
     try {
+      newArray = cart.map((product) => {
+        return {
+          title: product.title,
+          unit_price: parseFloat(product.price.toString()), // Asegúrate de que product.price sea un número
+          quantity: product.quantity,
+        };
+      });    
+      console.log(newArray); // Mover aquí la impresión
+  
       const response = await axios.post("https://backbarbara.vercel.app/create_preference", {
         items: newArray,
         shipment_cost: shippingCost
       });
-
+  
       const { id } = response.data;
       setPreferenceId(id);
     } catch (error) {
@@ -72,6 +53,7 @@ const MercadoPagoPayment = () => {
       setIsLoading(false);
     }
   };
+  
 
   return (
     <div style={{ textAlign: "center", marginTop: "20px", marginBottom: "20px", maxWidth: "300px", margin: "auto" }}>
